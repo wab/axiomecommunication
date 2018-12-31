3f4c6d8693dc0c73f4cc1d7d3627fe8ea0616973
wabdsgn <wab2com@gmail.com>
2018-12-31 17:12:35 +0100

    add logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,29 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
-import React from 'react'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Logo from './logo';
 
 const Header = ({ siteTitle }) => (
-  <div>{siteTitle}</div>
-)
+  <div
+    style={{
+      margin: `0 auto`,
+      maxWidth: 960,
+      padding: `2rem`,
+      display: 'flex',
+      justifyContent: 'center',
+    }}
+  >
+    <Logo width="350px" />
+  </div>
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
